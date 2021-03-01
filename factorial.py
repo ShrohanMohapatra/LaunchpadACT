bbc979043eb800b46a03212877ee960471582f82
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2021-03-01 12:16:47 -0500

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 from time import time
+from unittest import TestCase
 def fact(n):
     if n == 0: return 1
     else: return n*fact(n-1)
@@ -13,6 +14,16 @@ def movingaverage(arr,win):
         b.append(s/win)
     for k in range(len(arr)-int(win/2),len(arr)): b.append(arr[k])
     return b
+class testFrameWork(TestCase):
+    def test1(self):
+        A = {}
+        for k in range(998):
+            if k == 0: A[k] = 0
+            else:
+                A[k] = A[k-1]*k
+        flag = True
+        for k in range(998): flag = flag and fact(k) == A[k]
+        self.assertTrue(flag)
 A = [0 for i in range(998)]
 for k in range(len(A)):
     start = time()
