11dad98779297d992c5ae563cf1c6534f58f8bcd
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2021-06-11 19:36:01 +0530

    Update turingFile1.py

--- turingFile1.py
+++ turingFile1.py
@@ -1,5 +1,6 @@
+from math import ceil
 def turingMachine(Tape):
-  for i in range(int(len(Tape)/2)):
+  for i in range(int(ceil(len(Tape)/2))):
     if Tape[i]=='a' and Tape[len(Tape)-1-i]=='b': pass
     else: return False
   return True
@@ -7,3 +8,7 @@ assert turingMachine(['a','a','a','b','b','b'])==True
 assert turingMachine(['a','a','a','b','b'])==False
 assert turingMachine(['a','a','b','b','b'])==False
 assert turingMachine([])==True
+assert turingMachine('aaabbb')==True
+assert turingMachine('aaabb')==False
+assert turingMachine('aabbb')==False
+assert turingMachine('')==True
